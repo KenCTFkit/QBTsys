1b29b35874cfa6a465ce7bbc14b9e15b7a9f1a4a
kenman <kenman.0321@icloud.com>
2023-01-12 16:16:17 +0900

    20230112

--- QBTv1/formapp/urls.py
+++ QBTv1/formapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import SetQRparam
 
 urlpatterns = [
     path('QBTForm', views.TestView.as_view(),name="formpage"),
+    path('QRcode', SetQRparam ,name="qrcode"),
 ]
--- QBTv1/formapp/views.py
+++ QBTv1/formapp/views.py
@@ -2,6 +2,8 @@ from django.contrib import messages
 from django.views.generic.edit import FormView
 from formapp.forms import QBTForm
 from django.shortcuts import render
+from django.views.generic import View
+from .models import User
 
 class TestView(FormView):
     template_name = 'App_Folder_HTML/formpage.html'
@@ -12,3 +14,9 @@ class TestView(FormView):
         form.save()  # 保存処理など
         messages.add_message(self.request, messages.SUCCESS, '登録しました！')  # メッセージ出力
         return super().form_valid(form)
+
+
+def SetQRparam(request):
+    post = User(StudentID='170127', TemperatureA=39, TemperatureB=2, Q1=0, Q2=0, FreeText='FreeText')
+    post.save() 
+    return render(request, 'TopPage.html')
