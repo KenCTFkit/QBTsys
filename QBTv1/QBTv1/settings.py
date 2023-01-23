9377a3ff9e6d695a757ec8eda6859953e11c2a65
kenman <kenman.0321@icloud.com>
2023-01-23 12:16:03 +0900

    a

--- QBTv1/QBTv1/settings.py
+++ QBTv1/QBTv1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t83!@#utc-=tts@8jk-!0a=ux$av7+&#3hfmo4k^b^lf9gf1g3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "192.168.81.106"]
 
 
 # Application definition
