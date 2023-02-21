874cfd99a8a8e4fb949c86f1e2721295c1a16a65
Kenman <91576587+KenCTFkit@users.noreply.github.com>
2023-02-21 10:47:57 +0900

    Delete mySite directory

--- mySite/myapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http.response import HttpResponse
-from .forms import ContactForm
-
-def index_template(request):
-    form = ContactForm()
-    return render(request, 'index.html', {
-        'form': form,
-    })
